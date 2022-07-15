cd04cc76cf605ad0f30bfd434f65a79f2d18fbaf
Václav Piroutek <vasek.pirou@gmail.com>
2022-07-15 12:16:54 +0200

    new navbar

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -1,4 +1,5 @@
 import { Button, Flex, IconButton, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
 import { BsFacebook, BsInstagram } from "react-icons/bs";
 
@@ -28,18 +29,26 @@ export const NavbarMenu: React.FC<{
           gap="15px"
           p="15px"
         >
-          <Button fontSize="1.2rem" fontWeight="600">
-            Portfolio
-          </Button>
-          <Button fontSize="1.2rem" fontWeight="600">
-            Služby
-          </Button>
-          <Button fontSize="1.2rem" fontWeight="600">
-            Postprodukce
-          </Button>
-          <Button fontSize="1.2rem" fontWeight="600">
-            Kontakt
-          </Button>
+          <Link href="/">
+            <Button fontSize="1.2rem" fontWeight="600">
+              Portfolio
+            </Button>
+          </Link>
+          <Link href="/sluzby">
+            <Button fontSize="1.2rem" fontWeight="600">
+              Služby
+            </Button>
+          </Link>
+          <Link href="/postprodukce">
+            <Button fontSize="1.2rem" fontWeight="600">
+              Postprodukce
+            </Button>
+          </Link>
+          <Link href="/kontakt">
+            <Button fontSize="1.2rem" fontWeight="600">
+              Kontakt
+            </Button>
+          </Link>
           <Flex gap="20px" w="100%" justify="flex-start">
             <a
               href="https://www.instagram.com/nylacreativeagency"
